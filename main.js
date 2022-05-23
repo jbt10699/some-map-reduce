7dedf9670a9f9d46461ae19f9444012704cb6013
John <jbt10699@gmail.com>
2022-05-23 18:12:22 -0400

    Added my own custom some

--- main.js
+++ main.js
@@ -27,3 +27,17 @@ Array.prototype.myCustomReduce = function(callback, accumulate){
 let arr = [5,21,61,8,1];
 console.log(arr.myCustomReduce((acc,item) => acc+item, 0));
 */
+
+Array.prototype.myCustomSome = function(callback){
+    for(let i=0; i<this.length; i++){
+        if(callback(this[i])){
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+let arr = [1,11,3,11,5];
+console.log(arr.myCustomSome((item) => item%2===0));
+*/
