f0c94da0a69b1c9c7e92c606986e418e41a2058d
John <jbt10699@gmail.com>
2022-05-23 18:08:50 -0400

    Added my own custom reduce

--- main.js
+++ main.js
@@ -10,5 +10,20 @@ Array.prototype.myCustomFilter = function(callback){
     return newArray;
 }
 
+/*
 let arr = [1,3,2,4];
 console.log(arr.myCustomFilter((el) => el>1 ) );
+*/
+
+Array.prototype.myCustomReduce = function(callback, accumulate){
+//var accumulate =0;
+    for(let i=0; i<this.length; i++){
+        callback(accumulate = accumulate+this[i]);
+    }
+    return accumulate;
+}
+
+/*
+let arr = [5,21,61,8,1];
+console.log(arr.myCustomReduce((acc,item) => acc+item, 0));
+*/
